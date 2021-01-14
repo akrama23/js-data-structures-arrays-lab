653b0800a17ce328a64d12f0bd3fbe2b60680c6f
akrama23 <arundelfootball23@gmail.com>
2021-01-13 22:23:07 -0500

    commit tree 5da6d073dec3a67d4924bc58d49e15cd2ac1b41b

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+   function destructivelyAppendDriver(name) {
+       return drivers.push(name)
+   }
+   function destructivelyPrependDriver(name) {
+       return drivers.unshift(name)
+   }
+   function destructivelyRemoveLastDriver() {
+       return drivers.pop()
+       //.pop removes the last so no argument needed
+   }
+
+   function destructivelyRemoveFirstDriver() {
+       return drivers.shift()
+   }
+   function appendDriver(name) {
+       return [...drivers, name]
+   }
+   function prependDriver(name) {
+       return [name, ...drivers]
+   }
+   function removeLastDriver() {
+       return drivers.slice()
+   }
+   function removeFirstDriver() {
+       return drivers.slice()
+   }
